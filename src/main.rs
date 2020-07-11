d21fb1be5d06deaab1de65852016abd61d9c17cf
Juan Medina <jamedina@gmail.com>
2020-07-11 08:35:34 +0100

    adding bounce system

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,8 @@ fn main() -> amethyst::Result<()> {
         .with_bundle(TransformBundle::new())?
         .with_bundle(input_bundle)?
         .with(systems::PaddleSystem, "paddle_system", &["input_system"])
-        .with(systems::MoveBallsSystem, "ball_system", &[]);
+        .with(systems::MoveBallsSystem, "ball_system", &[])
+        .with(systems::BounceSystem, "bounce_system", &[]);
 
     let assets_dir = app_root.join("assets");
     let mut game = Application::new(assets_dir, PongGame, game_data)?;
